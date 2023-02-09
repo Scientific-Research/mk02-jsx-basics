62d8b30f43874768664c5fe176f316fafb3916fb
Maximilian <scientificresearch309@gmail.com>
2023-02-09 23:59:25 +0100

    onClick, onChange...

--- src/App.jsx
+++ src/App.jsx
@@ -22,39 +22,61 @@ if (a === 1 && b === 2) {
   console.log("a = b");
 }
 
+const Password = "123";
 function App() {
-  const [userOnline, setUserOnline] = useState(true);
+  const [userOnline, setUserOnline] = useState(false);
+  const [thePassword, setThePassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(thePassword);
+    if (thePassword === Password) {
+      setUserOnline(true);
+      console.log("OK");
+    }
+    setUserOnline(false);
+  };
+
   return (
-    <div className="App">
-      <h1>{title}</h1>
-      <form action="">
-        password: <input type="text" />
-      </form>
-      {/* Strange if Statement */}
-      {userOnline && <div>User is Online!</div>}
-      {/* <p>Test</p> */}
-      <hr />
-      {/* Ternary  strange if-else Statement: */}
-      {userOnline ? (
-        <div>User doesn't need to login!</div>
-      ) : (
-        <div>User needs to login</div>
-      )}
-      <hr />
-      {userOnline && <div>secret password: 345345</div>}
-      {userOnline ? (
-        <div className="info-online">file Exists! </div>
-      ) : (
-        <div className="info-offline">file Deleted! </div>
-      )}
-      <hr />
-      <h1>There are {Techbooks.length} books:</h1>
-      {Techbooks.filter((tcb) => tcb.title.includes("Linux")).map((tb) => (
-        <ul key={tb.id}>
-          <li>{tb.title}</li>
-        </ul>
-      ))}
-    </div>
+    <>
+      <div className="App">
+        <h1>{title}</h1>
+        <form>
+          password:
+          <input
+            type="text"
+            onChange={(e) => setThePassword(e.target.value)}
+            value={thePassword}
+          />
+          {/* {thePassword} */}
+          <button onClick={handleLogin}>Login</button>
+        </form>
+        {/* Strange if Statement */}
+        {userOnline && <div>User is Online!</div>}
+        {/* <p>Test</p> */}
+        <hr />
+        {/* Ternary  strange if-else Statement: */}
+        {userOnline ? (
+          <div>User doesn't need to login!</div>
+        ) : (
+          <div>User needs to login</div>
+        )}
+        <hr />
+        {/* {userOnline && <div>secret password: 345345</div>}
+        {userOnline ? (
+          <div className="info-online">file Exists! </div>
+        ) : (
+          <div className="info-offline">file Deleted! </div>
+        )} */}
+        <hr />
+        <h1>There are {Techbooks.length} books:</h1>
+        {Techbooks.filter((tcb) => tcb.title.includes("Linux")).map((tb) => (
+          <ul key={tb.id}>
+            <li>{tb.title}</li>
+          </ul>
+        ))}
+      </div>
+    </>
   );
 }
 
