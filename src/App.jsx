313cb0ac539dd0725bf5a4b88164a2da03bf363a
Maximilian <scientificresearch309@gmail.com>
2023-02-09 15:31:35 +0100

    if in React -> &&

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.scss";
 
 const title = "Our JSX Basics Lesson";
+const userOnline = true;
+
+const a = 1;
+const b = 2;
+
+if (a === 1 && b === 2) {
+  // {<div>{a === b}</div>};
+  // <p>a=b</p>;
+  console.log("a = b");
+}
+
 function App() {
   return (
     <div className="App">
       <h1>{title}</h1>
+      {userOnline && <div>User is Online!</div>}
       <p>Test</p>
     </div>
   );
