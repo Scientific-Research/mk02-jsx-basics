5fd8501a3aa2aa39e41c30ca9bd95a540b056c7a
Maximilian <scientificresearch309@gmail.com>
2023-02-09 22:18:55 +0100

    {Techbooks.map((tb) => (         <ul key={tb.id}>           <li>{tb.title}</li>         </ul>       ))}

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ function App() {
   return (
     <div className="App">
       <h1>{title}</h1>
-
       <form action="">
         password: <input type="text" />
       </form>
@@ -35,24 +34,25 @@ function App() {
       {userOnline && <div>User is Online!</div>}
       {/* <p>Test</p> */}
       <hr />
-
       {/* Ternary  strange if-else Statement: */}
       {userOnline ? (
         <div>User doesn't need to login!</div>
       ) : (
         <div>User needs to login</div>
       )}
-
       <hr />
       {userOnline && <div>secret password: 345345</div>}
-
       {userOnline ? (
         <div className="info-online">file Exists! </div>
       ) : (
         <div className="info-offline">file Deleted! </div>
       )}
       <hr />
-      
+      {Techbooks.map((tb) => (
+        <ul key={tb.id}>
+          <li>{tb.title}</li>
+        </ul>
+      ))}
     </div>
   );
 }
